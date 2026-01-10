556525a460781637676579b7c8a0244719caa58f
Abhilash Mishra <abhilash00003108884@gmail.com>
2026-01-10 16:13:31 +0530

    fix: Refactor CORS configuration to support multiple origins dynamically

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,11 +7,32 @@ app.get("/", (req, res) => {
     res.status(200).send("Backend API is running 🚀");
 });
 
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+// }));
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://view-i1.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        // allow server-to-server / Postman
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("CORS not allowed"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 /* ✅ Preflight support */
